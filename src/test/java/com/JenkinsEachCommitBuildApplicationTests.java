f671829569039d5416b3761ba55a1e8944318f91
simobend1995@gmail.com <simobend1995@gmail.com>
2019-12-12 16:25:22 +0100

    c

--- src/test/java/com/JenkinsEachCommitBuildApplicationTests.java
+++ src/test/java/com/JenkinsEachCommitBuildApplicationTests.java
@@ -3,11 +3,16 @@ package com;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @SpringBootTest
 class JenkinsEachCommitBuildApplicationTests {
 
     @Test
     void contextLoads() {
+
+        assertEquals("salam", "salam");
     }
 
 }
